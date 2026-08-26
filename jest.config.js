c803b135045f9c17c71ec726e08a2572de071c39
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-08-27 10:19:00 +1200

    fix ci

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src', '<rootDir>/test'],
+  roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
   passWithNoTests: true,
 };
